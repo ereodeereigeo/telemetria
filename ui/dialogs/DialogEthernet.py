fcdb5e696b3ecf762f8416eb6c7aaf066edd1fde
Sebastian <sebastian.cerda@usach.cl>
2016-03-22 19:10:51 -0300

    tareas andando

--- ui/dialogs/DialogEthernet.py
+++ ui/dialogs/DialogEthernet.py
@@ -6,4 +6,16 @@ class EthernetDialog(QtGui.QDialog, dialog_can_bridge.Ui_CanBridgeDialog):
 
     def __init__(self):
         super(EthernetDialog, self).__init__()
+        self.ip = ()
+        self.port = 0
         self.setupUi(self)
+        self.pushButton_2.clicked.connect(self.aceptar_dialogo)
+        self.pushButton.clicked.connect(self.cancelar_dialogo)
+
+    def aceptar_dialogo(self):
+        self.ip = (self.spinBox.value(), self.spinBox_2.value(), self.spinBox_3.value(), self.spinBox_4.value())
+        self.port = self.spinBox_5.value()
+        QtGui.QDialog.accept(self)
+
+    def cancelar_dialogo(self):
+        QtGui.QDialog.reject(self)
